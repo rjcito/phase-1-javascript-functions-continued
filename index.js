fe43b2d7c26a63afb6da8b84cddcc6d0333e0c1f
Roberto Jacquez <rjqz92@gmail.com>
2021-10-20 23:36:22 -0600

    commit tree 99efd255b2c8e8e96799eccf2630c4e04782950f

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Your code here
+function saturdayFun(defaultActivity = "roller-skate"){
+    return  `This Saturday, I want to ${defaultActivity}!`;
+}
+
+function mondayWork(dfltActivity = "go to the office"){
+    return `This Monday, I will ${dfltActivity}.`;
+}
+
+
+function wrapAdjective(flair = "*"){
+    return function innerFunction(parameter1 = "special"){
+        return `You are ${flair}${parameter1}${flair}!`; 
+
+    }
+}
